b18624f8e4919501d74ec58ee97562893ebd7e4a
TheDayC <daveconnolly90@gmail.com>
2020-11-08 16:19:42 +0000

    Added on some comments on where I think I went wrong.

--- src/helpers/square.ts
+++ src/helpers/square.ts
@@ -2,7 +2,7 @@ export function findWordSquare(words: string[], dimensions: number, sequence: st
 
     let square: string[] = [];
     const wordsTmp = ['moan', 'once', 'acme', 'need'];
-    let wordsTmp2 = wordsTmp;
+    let wordsTmp2 = wordsTmp; // Would replace with full dictionary ideally.
     
     for(const word of wordsTmp) {
         square = [];
@@ -10,9 +10,13 @@ export function findWordSquare(words: string[], dimensions: number, sequence: st
 
         for (let i = 1; i < square[0].length; i++) {
             const prefix = buildPrefix(square, i);
-            const candidates = findCanidates(word, wordsTmp2, i, prefix);
+            const candidates = findCanidates(word, wordsTmp2, i, prefix); 
             
             if (candidates) {
+                /*
+                * Something that I can't quite figure out with the recursion here.
+                * I know I need to recurse on multiple layers of candidates but can't quite execute it.
+                */
                 square.push(candidates[0]);
                 
                 if (square.length === dimensions) { 
